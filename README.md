f573d2533089be8302fb6deeca59b72ffadd10b2
Mvery <matheus.verissimo@outlook.com>
2022-05-17 04:16:52 -0300

    FAQ Accordion Challenge

---
+++ README.md
@@ -0,0 +1,29 @@
+# Frontend Mentor - FAQ accordion card
+
+Esta é uma solução para o [FAQ accordion card challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/faq-accordion-card-XlyjD0Oam).
+
+## Screenshots
+
+![App Screenshot Desktop](printscreen/desktop.png)
+
+<p align="center">
+<img src="printscreen/mobile.png">
+</p>
+
+## Links
+
+Link:
+
+## Aprendizados
+
+Neste projeto eu pude aplicar e treinar meus conhecimentos em CSS e JavaScript.
+
+## Construído com
+
+-   HTML5
+-   CSS3
+-   JavaScript
+
+## Autores
+
+-   [@Mveryy](https://github.com/Mveryy)
---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+let title = document.querySelectorAll(".accordion");
+let arrow = document.querySelectorAll(".arrow");
+let panel = document.querySelectorAll(".panel");
+
+title.forEach((item) => {
+    item.addEventListener("click", () => {
+        if (item.classList.contains("accordionActive")) {
+            item.classList.remove("accordionActive");
+            item.firstElementChild.classList.remove("arrowActive");
+            item.nextElementSibling.classList.remove("panelActive");
+        } else {
+            resetAll();
+            item.classList.add("accordionActive");
+            item.firstElementChild.classList.add("arrowActive");
+            item.nextElementSibling.classList.add("panelActive");
+        }
+    });
+});
+
+panel.forEach((item) => {
+    item.addEventListener;
+});
+
+// Voltar o estado do Title, Arrow e Text para o padrão.
+function resetAll() {
+    title.forEach((item) => {
+        item.classList.remove("accordionActive");
+    });
+
+    arrow.forEach((item) => {
+        item.classList.remove("arrowActive");
+    });
+
+    panel.forEach((item) => {
+        item.classList.remove("panelActive");
+    });
+}
